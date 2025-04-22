dc642ebfee8313dceda10a0c8115dc7f1002e0a9
fahimhybri05 <fahim.hybri@gmail.com>
2025-04-22 17:11:10 +0600

    Pulled from master

--- Dashboard/Backend/routes/api.php
+++ Dashboard/Backend/routes/api.php
@@ -28,4 +28,3 @@ Route::delete('/job-applications/{id}', [JobApplicationAPIController::class, 'de
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
