f68995296cc5ac2385cf803f7de6644062c64fca
fahimhybri05 <fahim.hybri@gmail.com>
2025-05-10 03:04:07 +0600

    Active card added

--- Dashboard/Frontend/src/app/info-footer/info-footer.component.ts
+++ Dashboard/Frontend/src/app/info-footer/info-footer.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { ContactInfoComponent } from '@app/contact-info/contact-info.component';
 import { SocialMediaComponent } from '@app/social-media/social-media.component';
+import { Ui5MainModule } from '@ui5/webcomponents-ngx';
 
 
 @Component({
   selector: 'app-info-footer',
   standalone: true,
-  imports: [ContactInfoComponent,SocialMediaComponent],
+  imports: [ContactInfoComponent,SocialMediaComponent,Ui5MainModule],
   templateUrl: './info-footer.component.html',
   styleUrl: './info-footer.component.css'
 })
 export class InfoFooterComponent {
+loading: boolean = false;
+isActive: boolean = false;
 
+
+
+
+toggleActive($event: any) {
+  if ($event.target.checked) {
+    this.isActive = true;
+  } else {
+    this.isActive = false;
+  }
+}
 }
