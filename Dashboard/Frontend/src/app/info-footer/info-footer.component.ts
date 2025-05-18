d683f637675aa69d60e9072021f8a58fc7ad2215
arahman74 <arahman.hybritech65@gmail.com>
2025-05-18 19:28:44 +0600

    fix dynamic update section issue using id

--- Dashboard/Frontend/src/app/info-footer/info-footer.component.ts
+++ Dashboard/Frontend/src/app/info-footer/info-footer.component.ts
@@ -3,64 +3,67 @@ import { ContactInfoComponent } from '@app/contact-info/contact-info.component';
 import { CommonService } from '@app/services/common-service/common.service';
 import { SocialMediaComponent } from '@app/social-media/social-media.component';
 import { Ui5MainModule } from '@ui5/webcomponents-ngx';
-import { ToastMessageComponent } from "../components/toast-message/toast-message.component";
-
+import { ToastMessageComponent } from '../components/toast-message/toast-message.component';
 
 @Component({
   selector: 'app-info-footer',
   standalone: true,
-  imports: [ContactInfoComponent, SocialMediaComponent, Ui5MainModule, ToastMessageComponent],
+  imports: [
+    ContactInfoComponent,
+    SocialMediaComponent,
+    Ui5MainModule,
+    ToastMessageComponent,
+  ],
   templateUrl: './info-footer.component.html',
-  styleUrl: './info-footer.component.css'
+  styleUrl: './info-footer.component.css',
 })
 export class InfoFooterComponent {
   @Output() IsOpenToastAlert = new EventEmitter<void>();
-loading: boolean = false;
-isActive:any;
-title: string = '';
+  loading: boolean = false;
+  isActive: any;
+  title: string = '';
+  webID: number = 0;
   ToastType: string = '';
 
-constructor(private commonService: CommonService) {
-
-}
-ngOnInit() {
-  this.getPermissions();
-}
-
-getPermissions() {
-  this.commonService.get(`WebPages(1)`, true).subscribe({
-    next: (response: any) => {
-      this.isActive = response.is_active;
-    },
-    error: (error: any) => {
-      console.error('Error updating FAQ:', error);
-    },
-  });
-}
-
-toggleActive($event: any) {
-  if ($event.target.checked) {
-    this.isActive = true;
-  } else {
-    this.isActive = false;
+  constructor(private commonService: CommonService) {}
+  ngOnInit() {
+    this.getPermissions();
   }
-}
-  updateData() {
 
-    const permissionData = {
-      is_active: this.isActive ? true : false,
-    };
-    this.ToastType = 'edit';
-
-
-    this.commonService.put(`WebPages(1)`, permissionData, true).subscribe({
+  getPermissions() {
+    this.commonService.get(`WebPages`, true).subscribe({
       next: (response: any) => {
-        this.IsOpenToastAlert.emit();
+        this.webID = response.value[0].id;
+        this.isActive = response.value[0].is_active;
       },
       error: (error: any) => {
-        console.error('Error updating FAQ:', error);
+        console.error('Error fetching project section:', error);
       },
     });
   }
 
+  toggleActive($event: any) {
+    if ($event.target.checked) {
+      this.isActive = true;
+    } else {
+      this.isActive = false;
+    }
+  }
+  updateData() {
+    const permissionData = {
+      is_active: this.isActive,
+    };
+    this.ToastType = 'edit';
+
+    this.commonService
+      .put(`WebPages(${this.webID})`, permissionData, true)
+      .subscribe({
+        next: (response: any) => {
+          this.IsOpenToastAlert.emit();
+        },
+        error: (error: any) => {
+          console.error('Error updating project section:', error);
+        },
+      });
+  }
 }
