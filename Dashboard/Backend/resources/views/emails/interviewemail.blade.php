b623528b599421d1c9890ab95106c6cc4e09c390
arahman74 <arahman.hybritech65@gmail.com>
2025-06-02 18:38:31 +0600

    pulled from master

--- Dashboard/Backend/resources/views/emails/interviewemail.blade.php
+++ Dashboard/Backend/resources/views/emails/interviewemail.blade.php
@@ -6,7 +6,7 @@
 <body>
     <ul>
         <li><strong>Full Name:</strong> {{ $emailList->name }}</li>
-        <li><strong>Email:</strong> {{ $emailList->email }}</li>
+        <li><strong>Email::</strong> {{ $emailList->email }}</li>
         <li><strong>Interview Location:</strong> <br> {{ $emailList->address }}</li>
         <li><strong>Interview Date:</strong> {{ $emailList->interview_date }}</li>
     </ul>
