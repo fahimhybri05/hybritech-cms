4784ea04008588565232de02225c91ee43d96dca
arahman74 <arahman.hybritech65@gmail.com>
2025-06-16 12:12:27 +0600

    solved filter issue using api

--- Dashboard/Backend/app/Http/Controllers/ProjectController.php
+++ Dashboard/Backend/app/Http/Controllers/ProjectController.php
@@ -56,21 +56,11 @@ class ProjectController extends Controller
     public function index(Request $request)
     {
 
-           $odataFilter = $request->input('$filter');
-            $isOdataRequest = !empty($odataFilter);
         $query = Project::query();
-             if ($isOdataRequest) {
-                if (str_contains($odataFilter, 'is_active eq true')) {
-                    $query->where('is_active', true);
-                } elseif (str_contains($odataFilter, 'is_active eq false')) {
-                    $query->where('is_active', false);
+         if ($request->has('is_active')) {
+             $isActive = filter_var($request->input('is_active'), FILTER_VALIDATE_BOOLEAN);
+             $query->where('is_active', $isActive);
                 }
-            } else {
-                if ($request->has('is_active')) {
-                    $isActive = filter_var($request->input('is_active'), FILTER_VALIDATE_BOOLEAN);
-                    $query->where('is_active', $isActive);
-                }
-            }
         if ($request->has('search')) {
             $searchTerm = $request->input('search');
             $query->where(function($q) use ($searchTerm) {
